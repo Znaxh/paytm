30ebca6517dbf5bbf8219a90ce7f28478287b69c
Znaxh <anuragpratapsingh.4845@gmail.com>
2024-07-23 22:56:01 +0530

    checkpoint test api's

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ## Build a basic version of PayTM
 
 
-- Salting and hashing
+- Salting and hashing -> done hashing and salting but need to test the api's
 - Frontend { Responsive }
 - Moving db to cloud
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,13 @@
 // backend/routes/user.js
 const express = require('express');
-
+const bcrypt = require('bcrypt')
 const router = express.Router();
 const zod = require("zod");
 const { User, Account } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
 const  { authMiddleware } = require("../middleware");
+const saltRound = 10;
 
 const signupBody = zod.object({
     username: zod.string().email(),
@@ -33,33 +34,37 @@ router.post("/signup", async (req, res) => {
         })
     }
 
-    const user = await User.create({
-        username: req.body.username,
-        password: req.body.password,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-    })
-    const userId = user._id;
-
-    await Account.create({
-        userId,
-        balance: 1 + Math.random() * 10000
-    })
-
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET);
-
-    res.json({
-        message: "User created successfully",
-        token: token
+    const password = req.body.password;
+    bcrypt.hash(password,saltRound,async function(err,hash){
+        const user = await User.create({
+            username: req.body.username,
+            password: hash,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+        })
+        
+            const userId = user._id;
+        
+            await Account.create({
+                userId,
+                balance: 1 + Math.random() * 10000
+            })
+        
+            const token = jwt.sign({
+                userId
+            }, JWT_SECRET);
+        
+            res.json({
+                message: "User created successfully",
+                token: token
+            })
     })
 })
 
 
 const signinBody = zod.object({
     username: zod.string().email(),
-	password: zod.string()
+	passwo33rd: zod.string()
 })
 
 router.post("/signin", async (req, res) => {
@@ -72,20 +77,22 @@ router.post("/signin", async (req, res) => {
 
     const user = await User.findOne({
         username: req.body.username,
-        password: req.body.password
     });
 
-    if (user) {
-        const token = jwt.sign({
-            userId: user._id
-        }, JWT_SECRET);
-  
-        res.json({
-            token: token
-        })
-        return;
-    }
-
+    bcrypt.compare(req.body.password,user.password,function(err,result){
+        if (result) {
+            const token = jwt.sign({
+                userId: user._id
+            }, JWT_SECRET);
+      
+            res.json({
+                token: token
+            })
+            return;
+        }else{
+            console.log(err);
+        }
+    })
     
     res.status(411).json({
         message: "Error while logging in"
@@ -105,13 +112,19 @@ router.put("/", authMiddleware, async (req, res) => {
             message: "Error while updating information"
         })
     }
-
-    await User.updateOne(req.body, {
-        id: req.userId
-    })
-
-    res.json({
-        message: "Updated successfully"
+    bcrypt.hash(req.body.password,saltRound,async function(err,result){
+        const checkBody = {
+            password: result,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+        }
+        await User.updateOne(checkBody, {
+            id: req.userId
+        })
+    
+        res.json({
+            message: "Updated successfully"
+        })
     })
 })
 
